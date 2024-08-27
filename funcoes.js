4a561cd4d1739098f43e9d1642966ae7f9e204ee
DiegoSarti <diegosuely18@gmail.com>
2024-08-27 14:17:14 -0300

    funcoes

---
+++ funcoes.js
@@ -0,0 +1,7 @@
+function exibeNomeEstudante(nome, nota) {
+    return `o nome é ${nome} e a nota é ${nota}`;
+}
+
+console.log(exibeNomeEstudante('Diego', 10));
+console.log(exibeNomeEstudante('Ana', 7));
+console.log(exibeNomeEstudante('Lucas',8));
