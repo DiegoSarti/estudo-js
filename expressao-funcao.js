49d6bce1f17328d424c82408ac38e74f4309bee6
DiegoSarti <diegosuely18@gmail.com>
2024-08-28 13:21:05 -0300

    expressão de função

--- expressao-funcao.js
+++ expressao-funcao.js
@@ -1,3 +1,15 @@
-let nome = 'Diego';
+ 
+ console.log(estudanteReprovou(6, 5));
+ console.log(estudanteReprovou(10, 2));
+ 
+ 
+ 
+ const estudanteReprovou = function (notaFinal, faltas) {
+    if (notaFinal < 7 && faltas > 4) {
+        return ture;
+        
+    } else {
+        return false;
+    }
+}
 
-console.log(nome);
