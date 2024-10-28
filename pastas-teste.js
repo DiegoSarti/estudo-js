2b63e782e1a86303900ecf5fef744ad269d6c089
joaocavanhass <joaopedro.cavanhascolombo@gmail.com>
2024-10-28 14:33:45 -0300

    exercicios até o 4

---
+++ pastas-teste.js
@@ -0,0 +1,22 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+// --------------------------------
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+// ------------------------------------
+// let soma = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     soma += i; 
+// }
+
+// console.log(soma);
+// --------------------------------
+// const frutas = ["maça","banana","melancia"];
+//     for (let i = 0; i < frutas.length; i++) {
+//     console.log(frutas[i]);
+// }
